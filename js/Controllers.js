9e5401093c04cf6245de777e51cb0ecebc6f1b7d
mohap710 <mohap710@gmail.com>
2023-02-08 09:44:25 +0200

    orgainize data structures in controllers

--- js/Controllers.js
+++ js/Controllers.js
@@ -1,5 +1,22 @@
 app.controller("appCtrl", function ($rootScope, $scope, $timeout, $interval) {
-
+    $rootScope.categories = [
+        {
+            img: "/img/svg/web design.svg", title: "Web Design", caption: "Sorem hpsum folor sixdsft amhtget, consectetur adipiscing eliht, sed do eiusmod temporincidi."
+        },
+        { img: "/img/svg/multimedia.svg", title: "Video Editing", caption: "Sorem hpsum folor sixdsft amhtget, consectetur adipiscing eliht, sed do eiusmod temporincidi." },
+        {
+            img: "/img/svg/laptop.svg", title: "Digital Marketing", caption: "Sorem hpsum folor sixdsft amhtget, consectetur adipiscing eliht, sed do eiusmod temporincidi."
+        },
+        {
+            img: "/img/svg/reading-book.svg", title: "App Development", caption: "Sorem hpsum folor sixdsft amhtget, consectetur adipiscing eliht, sed do eiusmod temporincidi."
+        },
+        {
+            img: "/img/svg/to-do-list.svg", title: "Seo Marketing", caption: "Sorem hpsum folor sixdsft amhtget, consectetur adipiscing eliht, sed do eiusmod temporincidi."
+        },
+        {
+            img: "/img/svg/lamp.svg", title: "Content Writing", caption: "Sorem hpsum folor sixdsft amhtget, consectetur adipiscing eliht, sed do eiusmod temporincidi."
+        }
+    ]
 })
 app.controller("HomeCtrl", function ($scope) {
     window.scrollTo(0, 0)
@@ -21,42 +38,27 @@ app.controller("HomeCtrl", function ($scope) {
     ]
 
     $scope.team = [{
-        "name": "Shawnee Lightbown",
-        "title": "Registered Nurse",
-        "img": "/img/team1.png"
+        name: "Shawnee Lightbown",
+        title: "Registered Nurse",
+        img: "/img/team1.png"
     }, {
-        "name": "Pavlov Casali",
-        "title": "Developer I",
-        "img": "/img/team2.png"
+        name: "Pavlov Casali",
+        title: "Developer I",
+        img: "/img/team2.png"
     }, {
-        "name": "Pietra Gwinnell",
-        "title": "Nurse",
-        "img": "/img/team3.png"
+        name: "Pietra Gwinnell",
+        title: "Nurse",
+        img: "/img/team3.png"
     }, {
-        "name": "Tynan Garrand",
-        "title": "Clinical Specialist",
-        "img": "/img/team4.png"
+        name: "Tynan Garrand",
+        title: "Clinical Specialist",
+        img: "/img/team4.png"
     }]
-    $scope.categories = [
-        {
-            img: "/img/svg/web design.svg", title: "Web Design", caption: "Sorem hpsum folor sixdsft amhtget, consectetur adipiscing eliht, sed do eiusmod temporincidi."
-        },
-        { img: "/img/svg/multimedia.svg", title: "Video Editing", caption: "Sorem hpsum folor sixdsft amhtget, consectetur adipiscing eliht, sed do eiusmod temporincidi." },
-        {
-            img: "/img/svg/laptop.svg", title: "Digital Marketing", caption: "Sorem hpsum folor sixdsft amhtget, consectetur adipiscing eliht, sed do eiusmod temporincidi."
-        },
-        {
-            img: "/img/svg/reading-book.svg", title: "App Development", caption: "Sorem hpsum folor sixdsft amhtget, consectetur adipiscing eliht, sed do eiusmod temporincidi."
-        },
-        {
-            img: "/img/svg/to-do-list.svg", title: "Seo Marketing", caption: "Sorem hpsum folor sixdsft amhtget, consectetur adipiscing eliht, sed do eiusmod temporincidi."
-        },
-        {
-            img: "/img/svg/lamp.svg", title: "Content Writing", caption: "Sorem hpsum folor sixdsft amhtget, consectetur adipiscing eliht, sed do eiusmod temporincidi."
-        }
-    ]
+
     
 })
+
+
 app.controller("coursesCtrl", function ($scope, $rootScope) {
     $(document).ready(function () {
         $("#form").hide(1)
@@ -64,62 +66,36 @@ app.controller("coursesCtrl", function ($scope, $rootScope) {
             e.preventDefault()
             $("#form").show(200)
         })
-        // $("#form").submit(function (e) {
-        //     e.preventDefault()
-        //     let text1 =$("input:first").val()
-        //     let text2 = $("input:last").val()
-        //     $("li.faqs").append("<article class=\"container-fluid uk-comment\">\n" +
-        //         "                <header class=\"uk-comment-header\">\n" +
-        //         "                    <div class=\"uk-grid-medium uk-flex-middle\" uk-grid>\n" +
-        //         "                        <div class=\"uk-width-auto\">\n" +
-        //         "                            <img class=\"uk-comment-avatar\" src=\"img/about1.png\" width=\"80\" height=\"80\" alt=\"\">\n" +
-        //         "                        </div>\n" +
-        //         "                        <div class=\"uk-width-expand\">\n" +
-        //         "                            <h4 class=\"uk-comment-title uk-margin-remove\"><a class=\"uk-link-reset\" href=\"#\">"+text1+"</a></h4>\n" +
-        //         "                            <ul class=\"uk-comment-meta uk-subnav uk-subnav-divider uk-margin-remove-top\">\n" +
-        //         "                                <li><a href=\"#\">12 days ago</a></li>\n" +
-        //         "                                <li><a id=\"reply\">Reply</a></li>\n" +
-        //         "                            </ul>\n" +
-        //         "                        </div>\n" +
-        //         "                    </div>\n" +
-        //         "                </header>\n" +
-        //         "                <div class=\"uk-comment-body\">\n" +
-        //         "                    <p>"+text2+".</p>\n" +
-        //         "                </div>\n" +
-        //         "\n" +
-        //         "            </article>")
-        //
-        //
-        // })
+
         $scope.add = function (name, comment) {
             $scope.faqs.push(name, comment)
         }
 
     })
-    $scope.web = [
+    $scope.courses = [
         {
-            name: "HTML", img: "img/html.png", duration: "7 Hours", diff: "easy", lectures: [
+            instructor:{name:"Doti Haig",img:"/img/team1.png"},price:500,category:"web",name: "HTML", img: "img/courses/html.png", duration: "7 Hours", level: "easy", lectures: [
                 { name: "lecture1", lessons: [{ name: "Html Basics Tags" }] },
                 { name: "lecture2", lessons: [{ name: "Attribute" }, { name: "Table" }, { name: "Table Attribute(colspan,rowspan)" }] }
             ]
         },
         {
-            name: "CSS", img: "img/css-3.png", duration: "7 Hours", diff: "easy", lectures: [
+            instructor:{name:"Mae Desseine",img:"/img/team1.png"},price:500,category:"web",name: "CSS", img: "img/courses/css-3.png", duration: "7 Hours", level: "easy", lectures: [
                 { name: "lecture1", lessons: [{ name: "Style Attribute" }] },
                 { name: "lecture2", lessons: [{ name: "colors" }, { name: "Fonts" }, { name: "Margin" }] }
             ]
         },
         {
-            name: "JavaScript Basics", img: "img/javascript.png", duration: "7 Hours", diff: "easy", lectures: [
+            instructor:{name:"Hartwell Mungan",img:"/img/team4.png"},price:500,category:"web",name: "JavaScript Basics", img: "img/courses/javascript.png", duration: "7 Hours", level: "easy", lectures: [
                 { name: "lecture1", lessons: [{ name: "Variables" }, { name: "Functions" }] },
                 { name: "lecture2", lessons: [{ name: "Conditions(IF,While)" }, { name: "For Loops" }, { name: "Arrays" }] }
             ]
         },
-        { name: "Jquery", img: "img/jquery.png", duration: "10 Hours", diff: "med" },
-        { name: "Bootstrap", img: "img/bootstrap.jpg", duration: "10 Hours", diff: "med" },
-        { name: "AngularJS", img: "img/angularjs.png", duration: "12 Hours", diff: "hard" },
-        { name: "UI-Kit", img: "img/uikit.png", duration: "11 Hours", diff: "hard" },
-        { name: "Advanced JavaScript", img: "img/advance.jpg", duration: "7 Hours", diff: "hard" }
+        { instructor:{name:"Frasco Tour",img:"/img/team2.png"},price:500,category:"web",name: "Jquery", img: "img/courses/jquery.png", duration: "10 Hours", level: "med" },
+        { instructor:{name:"Heida Blaik",img:"/img/team3.png"},price:500,category:"web",name: "Bootstrap", img: "img/courses/bootstrap.jpg", duration: "10 Hours", level: "med" },
+        { instructor:{name:"Damara Rapinett",img:"/img/team4.png"},price:500,category:"web",name: "AngularJS", img: "img/courses/angularjs.png", duration: "12 Hours", level: "hard" },
+        { instructor:{name:"Melisandra Yerson",img:"/img/team2.png"},price:500,category:"web",name: "UI-Kit", img: "img/courses/uikit.png", duration: "11 Hours", level: "hard" },
+        { instructor:{name:"Trula Haliburton",img:"/img/team1.png"},price:500,category:"web",name: "Advanced JavaScript", img: "img/courses/advance.jpg", duration: "7 Hours", level: "hard" }
     ]
 
     $scope.faqs = [
@@ -128,9 +104,9 @@ app.controller("coursesCtrl", function ($scope, $rootScope) {
 
     ]
 
-    $scope.showCourse = function (x) {
+    $scope.viewCourse = function (course) {
 
-        $rootScope.course = x
+        $rootScope.course = course
 
         window.location.replace("http://127.0.0.1:8080/#!/main/course");
         console.log($rootScope.course)
